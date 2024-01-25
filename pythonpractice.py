ef794b2f2c270901db8f7f446eec609e4a69fe27
Brodie Brock <brodiebrock@gmail.com>
2024-01-25 15:11:13 -0600

    python practice

--- pythonpractice.py
+++ pythonpractice.py
@@ -25,3 +25,26 @@ print("result is", first_last_same(numbers_x))
 
 numbers_y = [75, 65, 35, 75, 30]
 print("result is", first_last_same(numbers_y))
+
+# Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+def checkDriverAge():
+    age = input("What is your age?: ")
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+checkDriverAge()
+
+# Exercise Functions
+# Highest Even: Write a function to find the highest even number from the list.
+def highest_even(li):
+    evens = []
+    for item in li:
+        if not item % 2 and item not in evens:
+            evens.append(item)
+    return max(evens)
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
