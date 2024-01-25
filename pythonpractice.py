5979e6582180af5323fbac54a5b48de87c9685a5
Brodie Brock <brodiebrock@gmail.com>
2024-01-25 17:28:22 -0600

    python practice

--- pythonpractice.py
+++ pythonpractice.py
@@ -64,13 +64,28 @@ elif income <= 20000:
 else:
     # first 10,000
     tax_payable = 0
-
     # next 10,000 10% tax
     tax_payable = 10000 * 10 / 100
-
     # remaining 20%tax
     tax_payable += (income - 20000) * 20 / 100
 
 print("Total tax to pay is", tax_payable)
 
+#Given two lists, l1 and l2, write a program to create a third list l3 by picking an odd-index element from the list l1 and even index elements from the list l2.
+list1 = [3, 6, 9, 12, 15, 18, 21]
+list2 = [4, 8, 12, 16, 20, 24, 28]
+res = list()
+
+odd_elements = list1[1::2]
+print("Element at odd-index positions from list one")
+print(odd_elements)
+
+even_elements = list2[0::2]
+print("Element at even-index positions from list two")
+print(even_elements)
+
+print("Printing Final third list")
+res.extend(odd_elements)
+res.extend(even_elements)
+print(res)
 
