5c79fde853834037b0846a0ecd325fb34a0cbede
Brodie Brock <brodiebrock@gmail.com>
2024-01-25 15:43:08 -0600

    python practice

--- pythonpractice.py
+++ pythonpractice.py
@@ -48,3 +48,29 @@ def highest_even(li):
     return max(evens)
 
 print(highest_even([10, 2, 3, 4, 8, 11]))
+
+#Exercise 12: Calculate income tax for the given income by adhering to the rules below
+income = 45000
+tax_payable = 0
+print("Given income", income)
+
+if income <= 10000:
+    tax_payable = 0
+elif income <= 20000:
+    # no tax on first 10,000
+    x = income - 10000
+    # 10% tax
+    tax_payable = x * 10 / 100
+else:
+    # first 10,000
+    tax_payable = 0
+
+    # next 10,000 10% tax
+    tax_payable = 10000 * 10 / 100
+
+    # remaining 20%tax
+    tax_payable += (income - 20000) * 20 / 100
+
+print("Total tax to pay is", tax_payable)
+
+
