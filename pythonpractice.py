83c98c2b9951272d4b020c1b519645abca295713
Brodie Brock <brodiebrock@gmail.com>
2024-01-24 18:54:49 -0600

    python practice

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,17 +1,12 @@
 #Write a program to remove characters from a string starting from zero up to n and return a new string.
-def first_last_same(numberList):
-  print("Given list:", numberList)
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
 
-  first_num = numberList[0]
-  last_num = numberList[-1]
-
-  return first_num == last_num
-
-numbers_x = [10, 20, 30, 40, 10]
-print("result is", first_last_same(numbers_x))
-
-numbers_y = [75, 65, 35, 75, 30]
-print("result is", first_last_same(numbers_y))
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
 
 #Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
 def first_last_same(numberList):
